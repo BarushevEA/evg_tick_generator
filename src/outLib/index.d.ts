5043273f94257b28ddc3e8b2684b1fa285873bca
grif <b_evgeny@ukr.net>
2024-07-29 12:16:50 +0300

    build 2.3.12

---
+++ src/outLib/index.d.ts
@@ -0,0 +1,8 @@
+export { GTimeout } from "./GTimeout";
+export { GTimeoutOrdered } from "./GTimeoutOrdered";
+export { GInterval } from "./GInterval";
+export { GIntervalOrdered } from "./GIntervalOrdered";
+export { GAnimationFrame } from "./GAnimationFrame";
+export { GAnimationFrameOrdered } from "./GAnimationFrameOrdered";
+export { TickCounter } from "./TickCounter";
+export { GMeter } from "./GMeter";

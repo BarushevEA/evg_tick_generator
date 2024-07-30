cc403d58fbf88bab77a70a99fbcda764926ec11c
grif <b_evgeny@ukr.net>
2024-07-31 02:52:57 +0300

    Refactor Measure function for improved naming clarity
    Renamed parameters in the Measure function and enhanced naming conventions within its body for consistency. Also updated the package version to 2.4.17 to reflect these changes.

--- src/Libraries/TickGenerator/MethodDecorator.ts
+++ src/Libraries/TickGenerator/MethodDecorator.ts
@@ -3,7 +3,7 @@ import {IMeter} from "./Types";
 
 const gMeter = new GMeter();
 
-export function Measure(classNameOriginal?: string, gMeterOptional?: GMeter) {
+export function Measure(measurementName?: string, gMeterOptional?: GMeter) {
     const meter = gMeterOptional ? gMeterOptional : gMeter;
     meter.start();
 
@@ -13,20 +13,24 @@ export function Measure(classNameOriginal?: string, gMeterOptional?: GMeter) {
         descriptor: PropertyDescriptor
     ) {
         const originalMethod = descriptor.value;
-        let className = classNameOriginal || target.name || "";
+        let targetName = target.name || "this";
 
         const funcType1 = Object.prototype.toString.call(originalMethod);
         const funcType2 = originalMethod[Symbol.toStringTag];
         const isAsync = funcType1 === '[object AsyncFunction]' || funcType2 === 'AsyncFunction';
 
+        let decorated: any;
+
         if (isAsync) {
+            const decorationName = measurementName || `Async method: ${targetName}.${propertyKey}`;
             descriptor.value = function (...args: any[]) {
-                const decorated = meter.decorateAsync(`Async method: ${className}.${propertyKey}`, () => originalMethod.apply(this, args));
+                !decorated && (decorated = meter.decorateAsync(decorationName, () => originalMethod.apply(this, args)));
                 return decorated();
             };
         } else {
+            const decorationName = measurementName || `Sync method: ${targetName}.${propertyKey}`;
             descriptor.value = function (...args: any[]) {
-                const decorated = meter.decorate(`Sync method: ${className}.${propertyKey}`, () => originalMethod.apply(this, args));
+                !decorated && (decorated = meter.decorate(decorationName, () => originalMethod.apply(this, args)));
                 return decorated();
             };
         }
